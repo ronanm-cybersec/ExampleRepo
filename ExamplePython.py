ac980c5187030b99a516b6d9cd1ea2dc1d02b42f
ronanm-cybersec <rmolina3080@stu.neisd.net>
2024-10-04 11:10:56 -0700

    Update ExamplePython.py
    Testing Password

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,3 +1,8 @@
+
+def test_password(x):
+    print(x)
+
+
 #This is how we learned to concatenate strings in the ITF+ class.
 a = "String 1"
 b = "String 2"
@@ -11,7 +16,10 @@ print(f"x = {x} and y = {y}")
 #We could also use an f-string like this:
 print("c = {c} and d = {d}".format(c=1, d=2))
 
-job_list = {'john': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
+job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
 
 for name, job in job_list.items():
     print (f"{name} is a {job}")
+
+Password = 'SuperSecretAdminPass'
+test_password(Password)
